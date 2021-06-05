4f15c2944d1e3fa0da1d8ecd59a433c585a520b7
Nidhi Singh <nidhi232singh@gmail.com>
2021-06-06 03:40:28 +0530

    Chat Fragment Layout

--- app/src/main/java/com/example/nidhi/whatsappclone/MainActivity.kt
+++ app/src/main/java/com/example/nidhi/whatsappclone/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.nidhi.whatsappclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.tabs.TabLayoutMediator
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         viewPager.adapter = SlideScreenAdapter(this)
+
+        TabLayoutMediator(tabs, viewPager,
+            TabLayoutMediator.TabConfigurationStrategy { tab, position ->
+                when (position) {
+                    0 -> {
+                        tab.text = "CHATS"
+                    }
+                    1 -> {
+                        tab.text = "PEOPLE"
+                    }
+                }
+            }).attach()
     }
 }
